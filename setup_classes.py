ee962bffd7fc92fbcfc07e817ca7e01b4baf7d45
Rodrigo Alfonsin <ro@roalfonsin.com>
2023-04-15 07:51:46 -0600

    Created CHANGELOG.md, added item lists to Warrior, modified Item:  added comments and type info, created equip_item function for Warrior

--- setup_classes.py
+++ setup_classes.py
@@ -6,7 +6,16 @@ class Gear:
 
 no_gear = Gear(0, 0, "No gear")        
 
+#Item class that is used for every expendable item
+#item_type is a string from [hp_item, atk_item, def_item]
+class Item:    
+    def __init__(self, input_type: str, input_value: int, input_description: str):
+        self.item_type = input_type
+        self.item_value = input_value
+        self.description = input_description
+
 class Warrior:
+    
     def __init__(self, input_hp, input_attack, input_defense, input_description):
         self.max_hp_stat = input_hp
         self.attack_stat = input_attack
@@ -14,6 +23,18 @@ class Warrior:
         self.health_stat = self.max_hp_stat
         self.equipped_gear = no_gear
         self.description = input_description
+        self.bag_item_list = []
+        self.equipped_item_list = []
+    
+    def equip_item(self, item_to_equip: Item):
+        self.equipped_item_list.append(item_to_equip)
+        #On the next if's we modify the Warrior object stats
+        if item_to_equip.item_type == "hp_item":
+            self.health_stat += item_to_equip.item_value
+        if item_to_equip.item_type == "atk_item":
+            self.attack_stat += item_to_equip.item_value
+        if item_to_equip.item_type == "def_item":
+            self.defense_stat += item_to_equip.item_value
     
     def is_alive(self):
         if self.health_stat <= 0:
@@ -26,8 +47,3 @@ class Warrior:
         
         
 
-class Item:
-    def __init__(self, input_type, input_value, input_description):
-        self.item_type = input_type
-        self.item_value = input_value
-        self.description = input_description
--- text-based-adventure-i.py
+++ text-based-adventure-i.py
@@ -7,6 +7,7 @@ import turn_settup as ts
 
 first_player = sc.Warrior(100, 10, 10, "Player 1")
 first_monster = sc.Warrior(10, 15, 8, "First monster created")
+first_item = sc.Item("hp_item", 100, "")
 
 ts.battle(first_player, first_monster)
 
