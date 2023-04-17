a24c6f7a7ba6da9f5079dc376ffb8013e42c4087
Rodrigo Alfonsin <ro@roalfonsin.com>
2023-04-17 10:13:15 -0600

    Fixed add-time-to-battles

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -86,4 +86,9 @@
 7. Changed order of entry in init for Item
 8. Recorded info for items for easy_campaign
 9. Added items to bag
-10. merge, commit and delete branch
+10. merge, commit and delete branch 5317511
+
+6. Created branch for issue #3: add-time-to-battles
+7. Added time to battle in turn_settup.py
+8. Tested time in battle
+9. merge, commit and delete branch
--- turn_settup.py
+++ turn_settup.py
@@ -1,11 +1,15 @@
 import setup_classes as sc
+import time
 
 def battle(player: sc.Warrior, monster: sc.Warrior):
     while player.is_alive() and monster.is_alive():
         player.action_attack(monster)
         if monster.is_alive():
             monster.action_attack(player)
-        print(player.health_stat, monster.health_stat)
+        print(player.description, "HP:", player.health_stat, "-",  monster.description, "HP:", monster.health_stat)
+        print("...")
+        time.sleep(1.5)
+        
 
 def turn(player: sc.Warrior, monster: sc.Warrior):
     player.select_gear()
